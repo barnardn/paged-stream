2ded38fe577f116cbbeed9e495a44686d5cff1fa
Norm Barnard <norm@normbarnard.com>
2026-05-13 13:26:37 -0400

    more reshuffling of defns

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,3 @@ pub trait PageableStream {
     fn prev(&mut self) -> impl Future<Output = Option<&Self::Item>> + Send;
     fn next(&mut self) -> impl Future<Output = Option<&Self::Item>> + Send;
 }
-
-pub struct PagedStream<'a, T> {
-    index: usize,
-    source: &'a T,
-}
-
-impl<'a, T> PagedStream<'a, T> {
-    pub fn new(source: &'a T) -> Self {
-        PagedStream { index: 0, source }
-    }
-}
--- src/vec.rs
+++ src/vec.rs
@@ -1,9 +1,21 @@
-use crate::{PageableStream, PagedStream};
+use crate::PageableStream;
 
 // implemenation of PageableStream for vectors. not especially useful, just a
 // demo impl to validate the scaffolding.
 //
-impl<'a, T: Sync> PageableStream for PagedStream<'a, Vec<T>> {
+
+pub struct PagedVecStream<'a, T> {
+    index: usize,
+    source: &'a T,
+}
+
+impl<'a, T> PagedVecStream<'a, T> {
+    pub fn new(source: &'a T) -> Self {
+        PagedVecStream { index: 0, source }
+    }
+}
+
+impl<'a, T: Sync> PageableStream for PagedVecStream<'a, Vec<T>> {
     type Item = T;
 
     async fn prev(&mut self) -> Option<&Self::Item> {
@@ -30,13 +42,13 @@ impl<'a, T: Sync> PageableStream for PagedStream<'a, Vec<T>> {
 mod test {
     use crate::PageableStream;
 
-    use super::PagedStream;
+    use super::PagedVecStream;
 
     #[tokio::test]
     async fn basics() {
         let test_vec = vec![1, 2, 3];
 
-        let mut st = PagedStream::new(&test_vec);
+        let mut st = PagedVecStream::new(&test_vec);
         assert_eq!(st.prev().await, None);
         assert_eq!(st.next().await, Some(&1));
         assert_eq!(st.next().await, Some(&2));
